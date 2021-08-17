02d1bdd87fd15e4e239673486e1efb8b9f015875
ZhouXiongWang <179656308@qq.com>
2021-08-17 19:07:36 +0800

    lg

---
+++ jieduan2/mokuai2/Pytest/Testpy.py
@@ -0,0 +1,24 @@
+import pytest
+import yaml
+
+
+class Test_calc:
+    # string参数化
+    @pytest.mark.parametrize('a,b', [(1, 2), (2, 3), (3, 4)])
+    def test_jf(self, a, b):
+        print(a + b)
+
+    # list参数化
+    @pytest.mark.parametrize(['a', 'b'], [(1, 2), (2, 3), (3, 4)])
+    def test_jf1(self, a, b):
+        print(a + b)
+
+    # 元组参数化
+    @pytest.mark.parametrize(("a", "b"), [(1, 2), (2, 3), (3, 4)])
+    def test_jf2(self, a, b):
+        print(a + b)
+
+    # 引用yaml
+    @pytest.mark.parametrize(("a", "b","c"), yaml.safe_load(open("./Data.yaml")))
+    def test_jfyaml(self, a, b,c):
+        print(f"{a}+{b}={c}")
